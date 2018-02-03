47c63632982ac3b911b3a03853c7f8dc01bc473a
Ryan Yurkanin <ryurkanin@getguru.com>
2018-02-02 23:40:09 -0500

    updating release script and adding a gitignore

--- dist/react-sentinel.main.js
+++ dist/react-sentinel.main.js
@@ -2,126 +2,8 @@
 
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-var React = require('react');
-var React__default = _interopDefault(React);
-var propTypes = _interopDefault(require('prop-types'));
-
-var asyncGenerator = function () {
-  function AwaitValue(value) {
-    this.value = value;
-  }
-
-  function AsyncGenerator(gen) {
-    var front, back;
-
-    function send(key, arg) {
-      return new Promise(function (resolve, reject) {
-        var request = {
-          key: key,
-          arg: arg,
-          resolve: resolve,
-          reject: reject,
-          next: null
-        };
-
-        if (back) {
-          back = back.next = request;
-        } else {
-          front = back = request;
-          resume(key, arg);
-        }
-      });
-    }
-
-    function resume(key, arg) {
-      try {
-        var result = gen[key](arg);
-        var value = result.value;
-
-        if (value instanceof AwaitValue) {
-          Promise.resolve(value.value).then(function (arg) {
-            resume("next", arg);
-          }, function (arg) {
-            resume("throw", arg);
-          });
-        } else {
-          settle(result.done ? "return" : "normal", result.value);
-        }
-      } catch (err) {
-        settle("throw", err);
-      }
-    }
-
-    function settle(type, value) {
-      switch (type) {
-        case "return":
-          front.resolve({
-            value: value,
-            done: true
-          });
-          break;
-
-        case "throw":
-          front.reject(value);
-          break;
-
-        default:
-          front.resolve({
-            value: value,
-            done: false
-          });
-          break;
-      }
-
-      front = front.next;
-
-      if (front) {
-        resume(front.key, front.arg);
-      } else {
-        back = null;
-      }
-    }
-
-    this._invoke = send;
-
-    if (typeof gen.return !== "function") {
-      this.return = undefined;
-    }
-  }
-
-  if (typeof Symbol === "function" && Symbol.asyncIterator) {
-    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
-      return this;
-    };
-  }
-
-  AsyncGenerator.prototype.next = function (arg) {
-    return this._invoke("next", arg);
-  };
-
-  AsyncGenerator.prototype.throw = function (arg) {
-    return this._invoke("throw", arg);
-  };
-
-  AsyncGenerator.prototype.return = function (arg) {
-    return this._invoke("return", arg);
-  };
-
-  return {
-    wrap: function (fn) {
-      return function () {
-        return new AsyncGenerator(fn.apply(this, arguments));
-      };
-    },
-    await: function (value) {
-      return new AwaitValue(value);
-    }
-  };
-}();
-
-
-
-
+var react = require('react');
+var PropTypes = _interopDefault(require('prop-types'));
 
 var classCallCheck = function (instance, Constructor) {
   if (!(instance instanceof Constructor)) {
@@ -152,20 +34,6 @@ var createClass = function () {
 
 
 
-
-var _extends = Object.assign || function (target) {
-  for (var i = 1; i < arguments.length; i++) {
-    var source = arguments[i];
-
-    for (var key in source) {
-      if (Object.prototype.hasOwnProperty.call(source, key)) {
-        target[key] = source[key];
-      }
-    }
-  }
-
-  return target;
-};
 
 
 
@@ -209,23 +77,32 @@ var cancelCheck = window.cancelIdleCallback || window.cancelAnimationFrame;
 var Sentinel = function (_PureComponent) {
   inherits(Sentinel, _PureComponent);
 
-  function Sentinel() {
-    var _ref;
-
-    var _temp, _this, _ret;
-
+  function Sentinel(props) {
     classCallCheck(this, Sentinel);
 
-    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
+    var _this = possibleConstructorReturn(this, (Sentinel.__proto__ || Object.getPrototypeOf(Sentinel)).call(this));
 
-    return _ret = (_temp = (_this = possibleConstructorReturn(this, (_ref = Sentinel.__proto__ || Object.getPrototypeOf(Sentinel)).call.apply(_ref, [this].concat(args))), _this), _this.watchPID = null, _this.checkPID = null, _this.state = {}, _this.watch = function () {
+    _this.setLoopingFunctions = function (lowPriority) {
+      // Low priority means use requestIdleCallback
+      // fallback to requestAnimationFrame
+      _this.requestCheck = lowPriority ? requestIdleCallback || requestAnimationFrame : requestAnimationFrame;
+
+      _this.cancelCheck = lowPriority ? cancelIdleCallback || cancelAnimationFrame : cancelAnimationFrame;
+    };
+
+    _this.watchPID = null;
+    _this.checkPID = null;
+
+    _this.watch = function () {
       _this.watchPID = window.setTimeout(function () {
         _this.checkPID = requestCheck(_this.check);
       }, 0);
-    }, _this.check = function () {
-      var updates = watchToProps(_this.props, findRootDecoratedComponentReference(_this.decoratedComponentReference), _this.state);
+    };
+
+    _this.check = function () {
+      var observe = _this.props.observe;
+
+      var updates = observe(_this.props);
 
       if (!updates) {
         return _this.watch();
@@ -245,13 +122,28 @@ var Sentinel = function (_PureComponent) {
         _this.setState(updates);
       }
 
-      _this.watch();
-    }, _temp), possibleConstructorReturn(_this, _ret);
+      return _this.watch();
+    };
+
+    _this.setLoopingFunctions(props.lowPriority);
+    return _this;
   }
 
   createClass(Sentinel, [{
     key: 'componentDidMount',
     value: function componentDidMount() {
+      this.watch();
+    }
+  }, {
+    key: 'componentWillReceiveProps',
+    value: function componentWillReceiveProps(nextProps) {
+      if (nextProps.lowPriority === this.props.lowPriority) return;
+
+      // Make a clean slate, since we will be changing functions.
+      clearTimeout(this.watchPID);
+      this.cancelCheck(this.checkPID);
+
+      this.setLoopingFunctions(nextProps.lowPriority);
       this.watch();
     }
   }, {
@@ -263,23 +155,20 @@ var Sentinel = function (_PureComponent) {
   }, {
     key: 'render',
     value: function render() {
-      return React__default.createElement(
-        'div',
-        null,
-        'hello world'
-      );
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      var _this2 = this;
-
-      return React__default.createElement(WrappedComponent, _extends({ ref: function ref(component) {
-          _this2.decoratedComponentReference = component;
-        } }, this.props, this.state));
+      return this.props.render(this.state);
     }
   }]);
   return Sentinel;
-}(React.PureComponent);
+}(react.PureComponent);
+
+Sentinel.propTypes = {
+  observe: PropTypes.func.isRequired,
+  render: PropTypes.func.isRequired,
+  lowPriority: PropTypes.bool
+};
+
+Sentinel.defaultProps = {
+  lowPriority: false
+};
 
 module.exports = Sentinel;
