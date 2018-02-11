4d73bab62298115af32179ea8ef6258da9c61c2c
Ryan Yurkanin <ryurkanin@getguru.com>
2018-02-11 18:05:47 -0500

    routing on gh-pages

--- example/modules/App/App.js
+++ example/modules/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 import GithubCorner from 'components/GithubCorner/GithubCorner';
 import Navbar from 'components/Navbar/Navbar';
@@ -11,16 +11,19 @@ import 'styles/reset.scss';
 import 'styles/typebase.scss';
 import 'modules/App/App.scss';
 
+const goToIndex = () => <Redirect to="/react-sentinel/element-query-example" />;
+
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
         <GithubCorner />
-        <Route exact path="/" component={ElementQueryExample} />
+        <Route exact path="/" component={goToIndex} />
+        <Route exact path="/react-sentinel/" component={goToIndex} />
 
-        <Route path="/element-query-example" component={ElementQueryExample} />
-        <Route path="/animation-example" component={AnimationExample} />
+        <Route path="/react-sentinel/element-query-example" component={ElementQueryExample} />
+        <Route path="/react-sentinel/animation-example" component={AnimationExample} />
       </div>
     );
   }
