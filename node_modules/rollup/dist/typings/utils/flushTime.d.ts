bf364c072003c8a4ebec04a43b2cdce47f7308f7
Ryan Yurkanin <ryurkanin@getguru.com>
2018-02-11 17:17:18 -0500

    Setting up proper gitignore, removing dist and node_modules

--- node_modules/rollup/dist/typings/utils/flushTime.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function timeStart(label: string): void;
-export declare function timeEnd(label: string): void;
-export declare function flushTime(log?: typeof defaultLog): void;
-/** @interal */
-export declare function defaultLog(label: string, time: number): void;
--- node_modules/rollup/typings/package.json.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'package.json' {
-	const version: string;
-}
